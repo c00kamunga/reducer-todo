6bb0d7bf0676b002346a24cf0cd05353afc55e4a
c00kamunga <tsothalanti2@yahoo.com>
2020-04-21 19:57:37 -0700

    added an initial state array of objects, imported react and useReducer from react in reducer.js file

---
+++ todo/src/reducers/reducer.js
@@ -0,0 +1,31 @@
+import React, { useReducer } from 'react';
+
+
+
+
+
+
+const todoList = [
+  {
+    item: "Learn about reducers",
+    completed: false,
+    id: 3892987589,
+  },
+  {
+    item: "Learn about python",
+    completed: false,
+    id: 3892987589,
+  },
+  {
+    item: "Learn about redux",
+    completed: false,
+    id: 3892987589,
+  },
+  {
+    item: "Learn about context API",
+    completed: false,
+    id: 3892987589,
+  },
+];
+
+

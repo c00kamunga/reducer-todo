18abf1dcac514923cb8645bc88f9cc163f9081ac
c00kamunga <tsothalanti2@yahoo.com>
2020-04-22 18:16:11 -0700

    added an action for adding todos

--- todo/src/reducers/reducer.js
+++ todo/src/reducers/reducer.js
@@ -1,31 +1,25 @@
-import React, { useReducer } from 'react';
+export const initialState = {
+  todos: [
+    {
+      item: "Learn about reducers",
+      completed: false,
+      id: 3892987589,
+    },
+  ],
+  todoCount: 1,
+};
 
-
-
-
-
-
-const todoList = [
-  {
-    item: "Learn about reducers",
-    completed: false,
-    id: 3892987589,
-  },
-  {
-    item: "Learn about python",
-    completed: false,
-    id: 3892987589,
-  },
-  {
-    item: "Learn about redux",
-    completed: false,
-    id: 3892987589,
-  },
-  {
-    item: "Learn about context API",
-    completed: false,
-    id: 3892987589,
-  },
-];
-
-
+export const reducer = (state, action) => {
+  switch(action.type){
+    case "add-todo":
+      return {
+        ...state,
+        todos: [
+          ...state.todos,
+          { item: action. payload, complete: false, id: Date.now()},
+        ],
+        todoCount: state.todoCount + 1,
+      };
+      
+  }
+}
